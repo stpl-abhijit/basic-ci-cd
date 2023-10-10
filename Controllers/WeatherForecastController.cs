d1351d2835082ac99fee039bd634adcd5a728b6b
Udhayarajan J <udhayarajan080@gmail.com>
2023-10-10 17:09:14 +0530

    data pass it

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -30,5 +30,19 @@ namespace basic_ci_cd.Controllers
             })
             .ToArray();
         }
+
+
+        [HttpGet(Name = "NewOne")]
+        public IEnumerable<WeatherForecast> NewOne()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                //Just check ci cd
+            })
+            .ToArray();
+        }
     }
 }
